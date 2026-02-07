007041b336178aa2a11bd7f06ee1d37f5b703185
Adiya-Kenessova <adiya10kenessova@gmail.com>
2026-02-07 14:38:16 +0500

    Update 214.py

--- Practice 2/ejudge/214.py
+++ Practice 2/ejudge/214.py
@@ -6,7 +6,7 @@ uniq = []
 
 for i in nums: #4 1 1 3
     if i in uniq:
-        cnt[uniq.index(i)] +=1
+        cnt[uniq.index(i)] +=1 #if already in uniq  find its index: uniq.index(1) = 1 then this index will be in cnt
     else:
         uniq.append(i) 
         cnt.append(1)    
@@ -18,5 +18,6 @@ num = []
 for i in range(len(uniq)):
     if cnt[i] == max1:
         num.append(uniq[i])
+
 
 print(min(num))
