cdb26662759d9ab8ce59ff5d97fb3831251d7f56
Adiya-Kenessova <adiya10kenessova@gmail.com>
2026-02-16 22:11:57 +0500

    Add placeholder function definition with 'pass'
    Added a placeholder function definition using 'pass'.

--- Practice 3/w3school/functions/basic_functions.py
+++ Practice 3/w3school/functions/basic_functions.py
@@ -28,3 +28,8 @@ def get_greeting():
   return "Hello from a function"
 message = get_greeting()
 print(message)
+
+
+#Function definitions cannot be empty. If you need to create a function placeholder without any code, use the pass statement:
+def my_function():
+  pass
