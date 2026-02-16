df2a028d220e7b52fb26d7e56995adb80a86ce16
Adiya-Kenessova <adiya10kenessova@gmail.com>
2026-02-16 23:19:34 +0500

    Create class_methods.py

---
+++ Practice 3/w3school/classes/class_methods.py
@@ -0,0 +1,38 @@
+#Methods are functions that belong to a class. They define the behavior of objects created from the class.
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def greet(self):
+    print("Hello, my name is " + self.name)
+
+p1 = Person("Emil")
+p1.greet()
+
+# ! Note: All methods must have self as the first parameter. !
+
+#Create a method with parameters:
+class Calculator:
+  def add(self, a, b):
+    return a + b
+
+  def multiply(self, a, b):
+    return a * b
+
+calc = Calculator()
+print(calc.add(5, 3))
+print(calc.multiply(4, 7))
+
+
+#The __str__() method is a special method that controls what is returned when the object is printed:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name} ({self.age})"
+
+p1 = Person("Tobias", 36)
+print(p1)    #Tobias (36)
+
