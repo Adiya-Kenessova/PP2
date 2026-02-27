391bbc207dece45cf616772fa63cbe95e5d6fd29
Adiya-Kenessova <adiya10kenessova@gmail.com>
2026-02-27 19:54:03 +0500

    Create math.py

---
+++ Practice 4/ejudge/math.py
@@ -0,0 +1,30 @@
+#ex built-in
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+#ex
+x = abs(-7.25) #7.25
+print(x)
+
+#ex
+x = pow(4, 3) #64
+print(x)
+
+#ex
+import math
+x = math.sqrt(64)
+print(x)
+
+#ex
+import math
+x = math.ceil(1.4)
+y = math.floor(1.4)
+print(x) # returns 2
+print(y) # returns 1
+#ex
+import math
+x = math.pi
+print(x)
