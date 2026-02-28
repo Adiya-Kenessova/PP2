e3fddd2fc97c34204fc7e6c87e2b19ba55404e68
Adiya-Kenessova <adiya10kenessova@gmail.com>
2026-02-28 14:37:12 +0500

    Update 418.py

--- Practice 4/ejudge/418.py
+++ Practice 4/ejudge/418.py
@@ -1,7 +1,8 @@
-x1, y1 = map(float, input().split())
+x1, y1 = map(float, input().split())    #finds the intersection of the line connecting two points with the x-axis
 x2, y2 = map(float, input().split())
 
 x = (x1*y2 + x2*y1) / (y1+y2)
 y = 0.0
+
 
 print(f"{x:.10f} {y:.10f}")
